36c110d18ae96245048cd7c10af4f7d6e8772521
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-05-17 16:34:12 +0700

    feat: create database connection and migration

---
+++ database/connection.py
@@ -0,0 +1,76 @@
+import psycopg2
+
+
+class CreateDatabase:
+    def __init__(self, user, password, host, port):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.connection = psycopg2.connect(
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port
+        )
+        self.connection.autocommit = True
+        self.cursor = self.connection.cursor()
+
+    def create_database(self, db_name):
+        try:
+            query = f"CREATE DATABASE {db_name}"
+            self.cursor.execute(query)
+            print(f"Database {db_name} created successfully")
+            return True
+        except Exception:
+            return False
+
+    def __del__(self):
+        self.cursor.close()
+        self.connection.close()
+
+class Database:
+    def __init__(self, db_name, user, password, host, port):
+        self.db_name = db_name
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.connection = psycopg2.connect(
+            dbname=self.db_name,
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port
+        )
+        self.connection.autocommit = True
+        self.cursor = self.connection.cursor()
+
+    def create_table(self, table_name, columns):
+        query = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns})"
+        self.cursor.execute(query)
+
+    def drop_table(self, table_name):
+        query = f"DROP TABLE IF EXISTS {table_name}"
+        self.cursor.execute(query)
+
+    def insert(self, table_name, columns, values):
+        query = f"INSERT INTO {table_name} ({columns}) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        self.cursor.execute(query, values)
+
+    def select(self, table_name, columns, condition):
+        query = f"SELECT {columns} FROM {table_name} WHERE {condition}"
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def update(self, table_name, set, condition):
+        query = f"UPDATE {table_name} SET {set} WHERE {condition}"
+        self.cursor.execute(query)
+
+    def delete(self, table_name, condition):
+        query = f"DELETE FROM {table_name} WHERE {condition}"
+        self.cursor.execute(query)
+
+    def __del__(self):
+        self.cursor.close()
+        self.connection.close()
---
+++ database/migration.py
@@ -0,0 +1,86 @@
+from connection import Database, CreateDatabase
+import argparse
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Manage database tables.')
+parser.add_argument('operation', choices=[
+                    'create_database', 'create_table', 'drop'], help='The operation to perform.')
+# optional arguments
+parser.add_argument('--table', help='The name of the table to operate on.')
+args = parser.parse_args()
+
+# command-line arguments
+# create database
+# python migration.py create_database
+
+# create table users
+# python migration.py create_table --table users
+# drop table users
+# python migration.py drop --table users
+
+# create table attendance
+# python migration.py create_table --table attendance
+# drop table attendance
+# python migration.py drop --table attendance
+
+def connection():
+    try:
+        db = Database(
+            db_name="attendance_webapp",
+            user="postgres",
+            password="sundaya2023",
+            host="localhost",
+            port=5432
+        )
+        return db
+    except Exception as e:
+        print(e)
+        return False
+
+def create_table():
+    # database connection
+    db = connection()
+    try:
+        if args.table == 'users':
+            # table users
+            db.create_table(
+                table_name="users",
+                columns='id SERIAL PRIMARY KEY, npm VARCHAR, nama_lengkap VARCHAR, kelas VARCHAR, foto VARCHAR, "createdAt" TIMESTAMP DEFAULT NOW()',
+            )
+        
+        if args.table == 'attendance':
+            # table absensi
+            db.create_table(
+                table_name="attendance",
+                columns='id SERIAL PRIMARY KEY, npm VARCHAR, tanggal_absen VARCHAR, foto VARCHAR, "createdAt" TIMESTAMP DEFAULT NOW()',
+            )
+    except Exception as e:
+        print(e)
+        return False
+
+def drop_table():
+    # database connection
+    db = connection()
+    try:
+        db.drop_table(table_name=args.table)
+    except Exception as e:
+        print(e)
+        return False
+
+def create_database():
+    db = CreateDatabase(
+        user="postgres",
+        password="sundaya2023",
+        host="localhost",
+        port=5432
+    )
+    db.create_database(db_name="attendance_webapp")
+
+
+# Perform operation
+if args.operation == 'create_database':
+    create_database()
+if args.operation == 'create_table':
+    create_table()
+elif args.operation == 'drop':
+    drop_table()
