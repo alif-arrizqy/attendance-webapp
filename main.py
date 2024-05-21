ef13531850356c32ffa99660eb720d7ae23ecdc7
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-05-21 15:48:21 +0700

    add secret key

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 from views import views
+app.secret_key = '83dcdc455025cedcfe64b21e620564fb'
 app.register_blueprint(views, url_prefix="/")
 
 if __name__ == "__main__":
